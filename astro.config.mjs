423d86dd85eeffca1a19745a5e8e39f4f898f696
Charlotte Lichter <celichter@gmail.com>
2025-01-30 16:29:57 +0100

    secondcommit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import tailwind from '@astrojs/tailwind';
 import icon from "astro-icon";
 import { defineConfig, squooshImageService } from 'astro/config';
 
+
 export default defineConfig({
+  site: 'https://DeltaGr.github.io',
   integrations: [tailwind(), mdx(), icon({
     include: {
       mdi: ["*"]
